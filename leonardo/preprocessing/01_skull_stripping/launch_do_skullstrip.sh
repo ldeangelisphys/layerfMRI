a06c77f646d1018f67cf249475ccfa5774ce85db
leonardo <leonardo@leonardos-MacBook-Pro.local>
2020-11-12 03:38:25 +0100

    comm

---
+++ leonardo/preprocessing/01_skull_stripping/launch_do_skullstrip.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This script runs do_skullstrip.py for all subjects in parallel in the
+# conda environment py37
+# Check python do_skullstrip.py -h for information about the arguments
+
+# NB: pandoc should be installed on the system by sudo
+
+# initialize the proper environment to run the python script
+source activate py37
+echo
+echo `conda info | grep "active environment"`
+echo
+echo
+
+# subjects 1 4 7 have missing data
+subjects=(2 3 5 6 8 9 10 11 12 13 14)
+
+printf "%s\n" "${subjects[@]}" | xargs -n 1 -P 12 -I{} python do_skullstrip.py --sub={} --outroot=leonardo/layers/
+
+echo
+echo
